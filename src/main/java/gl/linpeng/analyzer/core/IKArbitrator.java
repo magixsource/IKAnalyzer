f8f120434f7e63c54bda83fe2bdaff8451ac2fe5
lin.peng <lin.peng@dtyunxi.com>
2020-01-05 21:23:08 +0800

    注释规范

--- src/main/java/gl/linpeng/analyzer/core/IKArbitrator.java
+++ src/main/java/gl/linpeng/analyzer/core/IKArbitrator.java
@@ -38,7 +38,7 @@ class IKArbitrator {
 
   /**
    * 分词歧义处理
-   * @param orgLexemes
+   * @param context
    * @param useSmart
    */
   void process(AnalyzeContext context, boolean useSmart) {
@@ -86,7 +86,6 @@ class IKArbitrator {
    * 歧义识别
    * @param lexemeCell 歧义路径链表头
    * @param fullTextLength 歧义路径文本长度
-   * @param option 候选结果路径
    * @return
    */
   private LexemePath judge(QuickSortSet.Cell lexemeCell, int fullTextLength) {
@@ -119,7 +118,8 @@ class IKArbitrator {
 
   /**
    * 向前遍历，添加词元，构造一个无歧义词元组合
-   * @param LexemePath path
+   * @param lexemeCell path
+   * @param option
    * @return
    */
   private Stack<QuickSortSet.Cell> forwardPath(QuickSortSet.Cell lexemeCell, LexemePath option) {
@@ -139,8 +139,8 @@ class IKArbitrator {
 
   /**
    * 回滚词元链，直到它能够接受指定的词元
-   * @param lexeme 
    * @param l
+   * @param option
    */
   private void backPath(Lexeme l, LexemePath option) {
     while (option.checkCross(l)) {
